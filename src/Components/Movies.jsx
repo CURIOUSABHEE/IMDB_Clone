a6c261328c69ff9a35128934bb45c0db5864bc2c
Abhishek Jamdade <jamdadeabhishek039@gmail.com>
2024-12-16 18:31:53 +0530

    fetch and displayed the movies from API & built the Movies Component Dynamically

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Moviecard from "./Moviecard";
+import axios from "axios";
 
 function Movies() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const apiKey = "a38613200c7f24ebeefc07af5f14cc15";
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+      )
+      .then((res) => {
+        if (Array.isArray(res.data.results)) {
+          setMovies(res.data.results);
+        } else {
+          console.error("Unexpected API response:", res.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <>
       <div className="text-2xl text-center mt-5 text-yellow-500 font-bold">
         Trending Movies
       </div>
-      <div className="flex flex-row justify-around">
-        <Moviecard />
-        <Moviecard />
-        <Moviecard />
-        <Moviecard />
-        <Moviecard />
-        <Moviecard />
-        <Moviecard />
-        <Moviecard />
+      <div className="grid grid-cols-8">
+        {movies.map((movieObj) => {
+          return (
+            <Moviecard
+              key={movieObj.id}
+              poster_path={movieObj.poster_path}
+              original_title={movieObj.original_title}
+            />
+          );
+        })}
       </div>
     </>
   );
 }
+//https://api.themoviedb.org/3/movie/popular?api_key=a38613200c7f24ebeefc07af5f14cc15&language=en-US&page=1
 
 export default Movies;
