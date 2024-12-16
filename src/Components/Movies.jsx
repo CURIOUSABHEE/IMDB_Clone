0f78a46d985f4ffde014654ae5dcad5a2947dbf1
Abhishek Jamdade <jamdadeabhishek039@gmail.com>
2024-12-16 13:04:36 +0530

    Built First MovieCard

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Moviecard from "./Moviecard";
 
 function Movies() {
   return (
-    <div>
-      <h1>Movies</h1>
-    </div>
+    <>
+      <div className="text-2xl text-center mt-5 text-yellow-500 font-bold">
+        Trending Movies
+      </div>
+      <div className="flex flex-row justify-around">
+        <Moviecard />
+        <Moviecard />
+        <Moviecard />
+        <Moviecard />
+        <Moviecard />
+        <Moviecard />
+        <Moviecard />
+        <Moviecard />
+      </div>
+    </>
   );
 }
 
