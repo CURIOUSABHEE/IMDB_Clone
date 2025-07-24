28aec4205729e7d3d5461cecfee43f6cbb427398
AbhishekJamdade <jamdadeabhishek039@gmail.com>
2025-07-24 18:16:29 +0530

    Updated the Banner Img

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import marvelsImg from "../assets/marvels.jpg"; // adjust path if needed
 
 function Banner() {
   return (
     <div
       className="h-[50vh] md:h-[60vh] bg-cover bg-center flex items-end"
-      style={{
-        backgroundImage: `url(https://shorturl.at/dv1Th)`,
-      }}
+      style={{ backgroundImage: `url(${marvelsImg})` }}
     >
       <div className="text-white text-2xl text-center bg-gray-900/40 w-full  p-2">
         Avengers Assemble
